b8464f00db568f2ae0bc3017c1244b668751c0bb
Roy Hyunjin Han <rhh@crosscompute.com>
2018-02-17 19:59:13 +0000

    Update dependencies

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst', 'CHANGES.rst'])
 setup(
     name='crosscompute-notebook-extensions',
-    version='0.5.2',
+    version='0.5.3',
     description='CrossCompute extensions for Jupyter Notebook',
     long_description=DESCRIPTION,
     classifiers=[
@@ -29,9 +29,9 @@ setup(
         'pytest-runner',
     ],
     install_requires=[
-        'invisibleroads-macros>=0.9.3',
-        'crosscompute>=0.7.5',
-        'crosscompute-types>=0.7.5',
+        'invisibleroads-macros>=0.9.4.3',
+        'crosscompute>=0.7.6',
+        'crosscompute-types>=0.7.6',
         'configparser',
         'jinja2',
         'mock',
